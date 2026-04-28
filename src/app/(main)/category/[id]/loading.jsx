34af6556d93c8c46d7c13898928a4583de857033
farjuu333 <farjanapinky350@gmail.com>
2026-04-28 15:11:29 +0600

    added newscard and loading

---
+++ src/app/(main)/category/[id]/loading.jsx
@@ -0,0 +1,13 @@
+const LoadingPage = () => {
+    return (
+        <div className='flex h-[85vh] items-center justify-center'>
+             News by category loading
+           <span className="loading loading-dots loading-xl"></span>
+            
+            </div>
+           
+        
+    );
+};
+
+export default LoadingPage;
--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,4 +1,5 @@
 import LeftSidebar from '@/components/homepage/news/LeftSidebar';
+import NewsCard from '@/components/homepage/news/NewsCard';
 import RightSidebar from '@/components/homepage/news/RightSidebar';
 import { getCategories, getNewsByCategoryId } from '@/lib/data';
 
@@ -17,11 +18,11 @@ const NewsCategoryPage = async({params}) => {
       <div className="  col-span-3">
         <LeftSidebar categories={categories} activeId={id}></LeftSidebar>
       </div>
-      <div className="font-bold   col-span-6">
+      <div className="col-span-6">
         <h2 className='font-bold text-lg'>News by category </h2>
        <div className='space-y-4 mt-6'>
         {news.length >0? news.map((n)=>{
-            return <div key={n._id} className="p-6 rounded-md border">{n.title}</div>
+            return <NewsCard key={n._id} news={n}></NewsCard>
           }):<h2 className='font-bold text-4xl text-center my-7'>No News Found</h2>}
        </div>
       </div>
